990f54e5e6df7b0eeb0d955de4b0317c1366b857
Ian Kruper <ijk@adsi-cni.com>
2016-06-12 18:47:50 -0400

    reads Arduino voltage over serial

--- EV Diagnostic Tool/MainScreen.Designer.cs
+++ EV Diagnostic Tool/MainScreen.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EV_Diagnostic_Tool
 {
-    partial class Form1
+    partial class MainScreen
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,133 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonScan = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxVoltage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBarStatus = new System.Windows.Forms.ProgressBar();
+            this.timerSample = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.progressBarStatus);
+            this.panel1.Controls.Add(this.labelStatus);
+            this.panel1.Controls.Add(this.buttonScan);
+            this.panel1.Location = new System.Drawing.Point(13, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(259, 59);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonScan
+            // 
+            this.buttonScan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonScan.Location = new System.Drawing.Point(3, 28);
+            this.buttonScan.Name = "buttonScan";
+            this.buttonScan.Size = new System.Drawing.Size(95, 28);
+            this.buttonScan.TabIndex = 0;
+            this.buttonScan.Text = "Scan";
+            this.buttonScan.UseVisualStyleBackColor = true;
+            this.buttonScan.Click += new System.EventHandler(this.buttonScan_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(6, 5);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(171, 20);
+            this.labelStatus.TabIndex = 1;
+            this.labelStatus.Text = "Controller not detected";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.textBoxVoltage);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(13, 88);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(258, 83);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Battery Voltage";
+            // 
+            // textBoxVoltage
+            // 
+            this.textBoxVoltage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxVoltage.Location = new System.Drawing.Point(10, 44);
+            this.textBoxVoltage.Name = "textBoxVoltage";
+            this.textBoxVoltage.Size = new System.Drawing.Size(115, 26);
+            this.textBoxVoltage.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(131, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Volts";
+            // 
+            // progressBarStatus
+            // 
+            this.progressBarStatus.Location = new System.Drawing.Point(104, 29);
+            this.progressBarStatus.Name = "progressBarStatus";
+            this.progressBarStatus.Size = new System.Drawing.Size(148, 26);
+            this.progressBarStatus.TabIndex = 2;
+            // 
+            // timerSample
+            // 
+            this.timerSample.Interval = 250;
+            this.timerSample.Tick += new System.EventHandler(this.timerSample_Tick);
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "MainScreen";
+            this.ShowIcon = false;
+            this.Text = "EV Diagnostic Tool";
+            this.Load += new System.EventHandler(this.MainScreen_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonScan;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxVoltage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBarStatus;
+        private System.Windows.Forms.Timer timerSample;
     }
 }
 
--- EV Diagnostic Tool/MainScreen.cs
+++ EV Diagnostic Tool/MainScreen.cs
@@ -3,18 +3,94 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EV_Diagnostic_Tool
 {
-    public partial class Form1 : Form
+    public partial class MainScreen : Form
     {
-        public Form1()
+        SerialPort controller;
+
+        public MainScreen()
         {
             InitializeComponent();
+        }
+
+        private void MainScreen_Load(object sender, EventArgs e)
+        {
+            FindController();
+        }
+
+        private void buttonScan_Click(object sender, EventArgs e)
+        {
+            FindController();
+        }
+
+        private void timerSample_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                controller.WriteLine("VOLTAGE 1");
+                Thread.Sleep(10);
+                textBoxVoltage.Text = controller.ReadTo("\r\n");
+            }
+            catch (Exception)
+            {
+                timerSample.Stop();
+                //TODO: close the port and so forth...
+            }
+        }
+
+        private void FindController()
+        {
+            controller = null;
+            labelStatus.Text = "Searching for controller...";
+            string[] ports = SerialPort.GetPortNames();
+            progressBarStatus.Value = 0;
+            progressBarStatus.Maximum = ports.Length;
+            progressBarStatus.Step = 1;
+
+            SerialPort temp = new SerialPort();
+            temp.ReadTimeout = 250;
+            temp.BaudRate = 9600;
+            temp.DtrEnable = true;
+            temp.RtsEnable = true;
+            foreach (string port in ports)
+            {
+                temp.PortName = port;
+                temp.Open();
+                temp.WriteLine("EV CONTROLLER");
+                Thread.Sleep(10);
+                try
+                {
+                    if (temp.ReadTo("\r\n") == "EV Controller")
+                    {
+                        controller = temp;
+                        break;
+                    }
+                }
+                catch (TimeoutException)
+                {
+                    progressBarStatus.PerformStep();
+                    temp.Close();
+                }
+            }
+            progressBarStatus.Value = 0;
+            if (controller == null)
+            {
+                labelStatus.Text = "Controller not detected";
+                timerSample.Stop();
+            }
+            else
+            {
+                labelStatus.Text = "Controller detected on " + controller.PortName;
+                timerSample.Start();
+            }
         }
     }
 }
